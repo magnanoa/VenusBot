d3d524dbab735e6f26616a2991653f271a7f7f71
zut <ahilton@gmail.com>
2017-10-12 00:18:07 +1100

    small steps

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ A simple echo bot for the Microsoft Bot Framework.
 var restify = require('restify');
 var builder = require('botbuilder');
 
+var luis = require('./luis.json');
+
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
@@ -35,3 +37,163 @@ var bot = new builder.UniversalBot(connector, function (session) {
 
 const LuisModelUrl = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/40bb5710-6605-411c-b2e4-def1113b8ead?subscription-key=8363b631fdc04d2eb76db7ecdd1595e2&staging=true&verbose=true&timezoneOffset=0&q='
 
+console.log(LuisModelUrl)
+var recognizer = new builder.LuisRecognizer(LuisModelUrl);
+bot.recognizer(recognizer);
+
+bot.dialog('Order', [
+    function (session, args, next) {
+        var order = session.dialogData.order = {}
+
+        if (args && args.isReprompt && args.dialogData && args.dialogData.order){
+            // We were sent here to revalidate some user input
+            // Reinitialise the order data using the args
+            order = session.dialogData.order = args.dialogData.order
+        }
+        else if (args && args.intent && args.intent.entities){
+            // LUIS recognizer triggered dialog
+            // Scrape out all intent entities
+            var {intent} = args
+            console.log(intent.entities)
+
+            var stock = builder.EntityRecognizer.findEntity(intent.entities, 'Stocks')
+            if (stock && stock.resolution){
+                order.stock = stock.resolution.values[0]
+            }
+
+            var quantity = builder.EntityRecognizer.findEntity(intent.entities, 'builtin.number')
+            if (quantity && quantity.resolution){
+                order.qty = quantity.resolution.value
+            }
+
+            var direction = builder.EntityRecognizer.findEntity(intent.entities, 'OrderDirection')
+            if (direction && direction.entity){
+                order.direction = direction.entity
+            }
+        }
+
+        if (!order.stock) {
+            builder.Prompts.text(session, 'What stock would you like to order?', {
+                speak: 'What stock would you like to order?',
+                retrySpeak: 'What stock would you like to order? Say cancel to dismiss me',
+                inputHint: builder.InputHint.expectingInput
+            })
+        } else {
+            next()
+        }
+
+    },
+    /*
+        --1-- Validate stock
+     */
+    function (session, results, next) {
+        var {dialogData} = session
+        var {order} = dialogData
+
+        if (!order.stock) {
+            // Example response: { index: 0, entity: 'Apple', score: 0.8 }
+            const bestMatch = builder.EntityRecognizer.findBestMatch(getStockListFromLuisConfig(), results.response, 0.7)
+            console.log(bestMatch)
+            if (bestMatch){
+                order.stock = bestMatch.entity
+            }
+        }
+
+        if (!order.stock) {
+            // Unable to validate stock, send back to the start...
+            session.replaceDialog('Order', {dialogData: dialogData, isReprompt: true});
+        }
+        else if (!order.qty){
+            builder.Prompts.number(session, 'How many '+order.stock+' would you like to order?')
+        } else {
+            next()
+        }
+
+    },
+    /*
+        --2-- Validate qty
+     */
+    function (session, results, next) {
+        var {dialogData} = session
+        var {order} = dialogData
+
+        if (!order.qty) {
+            order.qty=results.response
+        }
+
+        if (!order.direction){
+            builder.Prompts.choice(session, 'Would you like to buy or sell '+order.stock+'?', ['Buy','Sell'],
+                { listStyle: builder.ListStyle.button })
+        } else {
+            next()
+        }
+
+    },
+    /*
+        --3-- Validate direction
+     */
+    function (session, results) {
+        var {dialogData} = session
+        var {order} = dialogData
+
+        if (!order.direction) {
+            // check the confirmation response
+            console.log(results.response)
+            if (results.response.entity){
+                order.direction=results.response.entity
+            }
+            else {
+                console.log('Something went wrong, I shouldn\'t end up here')
+            }
+
+        }
+
+        builder.Prompts.text(session, 'Placing a ['+order.direction+'] order for ['+order.qty+'] of ['+order.stock+']...');
+
+    }
+]).triggerAction({
+    matches: 'Order',
+    /*TODO:: disable confirmation prompt to avoid 'ibm'/'microsoft' stock confirmation triggering unwanted new dialog confirmation*/
+    //confirmPrompt: "This will cancel the creation of order you started. Are you sure?"
+}).cancelAction('cancelCreateNote', "Order canceled.", {
+    matches: /^(cancel|nevermind)/i,
+    confirmPrompt: "Are you sure you want to stop ordering?"
+});
+
+
+
+/*
+* "closedLists": [
+    {
+      "name": "Stocks",
+      "subLists": [
+        {
+          "canonicalForm": "Apple",
+          "list": []
+        },
+        {
+          "canonicalForm": "Optus",
+          "list": []
+        },
+        {
+          "canonicalForm": "Microsoft",
+          "list": []
+        },
+        {
+          "canonicalForm": "sony",
+          "list": []
+        },
+        {
+          "canonicalForm": "dell",
+          "list": []
+        },
+        {
+          "canonicalForm": "ibm",
+          "list": []
+        }
+      ]
+    }*/
+function getStockListFromLuisConfig() {
+    const stockList = luis.closedLists.filter(list=>list.name === 'Stocks')[0]
+    return stockList.subLists.map(element=>element.canonicalForm)
+}
